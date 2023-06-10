18e4556e09e88f819b008b98f00cfb4fb8d7e126
Roblify <127715757+Roblify@users.noreply.github.com>
2023-06-10 16:19:55 -0700

    Update postcommand.js

--- postcommand.js
+++ postcommand.js
@@ -1,4 +1,4 @@
-// All rights reserved. Please do not steal the code from the open source repository on Github.
+// All rights reserved. Please do not steal the code from the open source.
 
 require("dotenv/config")
 const { SlashCommandBuilder, EmbedBuilder, Client, GatewayIntentBits, ModalBuilder, ActionRowBuilder, TextInputBuilder, TextInputStyle, Events } = require("discord.js");
@@ -111,4 +111,4 @@ const commands = [
     }
 });
 
-// All rights reserved. Please do not steal the code from the open source repository on Github.
+// All rights reserved. Please do not steal the code from the open source.
