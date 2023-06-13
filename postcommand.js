93797f77790f56b902e413713c3f11ced1b52b62
Roblify <127715757+Roblify@users.noreply.github.com>
2023-06-12 17:07:42 -0700

    Update postcommand.js

--- postcommand.js
+++ postcommand.js
@@ -29,7 +29,7 @@ const commands = [
     .addStringOption(option => option.setName('job-type').setDescription('Select job type').addChoices({ name: 'For-Hire', value: 'for-hire' }, { name: 'Hiring', value: 'hiring' }, { name: 'Selling', value: 'selling' }).setRequired(true)),
 ];
 
-  client.on('interactionCreate', async interaction => {
+client.on('interactionCreate', async interaction => {
     if (!interaction.isCommand()) return;
 
     if (interaction.commandName === 'post') {
@@ -39,19 +39,32 @@ const commands = [
         return interaction.reply(`You have to wait ${remainingTime} more hours before using this command again.`);
       }
 
-      let channelId = null;
+      const approveChannelId = '1117185592690741278';
 
-      if (interaction.options.getString('job-type') == 'hiring') {
+      let channelId = null;
+      let paymentType = null;
+
+      if (interaction.options.getString('job-type') === 'hiring') {
         channelId = '1073004427893555240';
-      } else if (interaction.options.getString('job-type') == 'for-hire') {
+      } else if (interaction.options.getString('job-type') === 'for-hire') {
         channelId = '1073004365008343091';
-      } else if (interaction.options.getString('job-type') == 'selling') {
-        channelId = '1062218093377617941'
+      } else if (interaction.options.getString('job-type') === 'selling') {
+        channelId = '1062218093377617941';
+      }
+
+      if (interaction.options.getString('payment-type') === 'upon-completion') {
+        paymentType = 'Upon-Completion';
+      } else if (interaction.options.getString('payment-type') === 'upfront') {
+        paymentType = 'Upfront';
+      } else if (interaction.options.getString('payment-type') === 'partial-upfront') {
+        paymentType = 'Partial-Upfront';
+      } else if (interaction.options.getString('payment-type') === 'per-task') {
+        paymentType = 'Per-Task';
       }
 
       const modal2 = new ModalBuilder()
         .setTitle("Post Configuration")
-        .setCustomId('modal2')
+        .setCustomId('modal2');
 
       const Title = new TextInputBuilder()
         .setCustomId("Title")
@@ -68,13 +81,71 @@ const commands = [
       const Payment = new TextInputBuilder()
         .setCustomId("Payment")
         .setRequired(true)
+        .setLabel("Provide the payment (Robux, USD, Other)")
+        .setStyle(TextInputStyle.Short);
+
+      const ImageUrl = new TextInputBuilder()
+        .setCustomId("ImageUrl")
+        .setRequired(false)
+        .setLabel("Image URL (Portfolio, game, etc...)")
+        .setStyle(TextInputStyle.Short);
+
+      const approveButton = new ButtonBuilder()
+        .setCustomId('approve_button')
+        .setLabel('Approve')
+        .setStyle(ButtonStyle.Success);
+
+      const declineButton = new ButtonBuilder()
+        .setCustomId('decline_button')
+        .setLabel('Decline')
+        .setStyle(ButtonStyle.Danger);
+
+      const firstActionRow = new ActionRowBuilder().addComponents(Title);
+      const secondActionRow = new ActionRowBuilder().addComponents(Description);
+      const thirdActionRow = new ActionRowBuilder().addComponents(Payment);
+      const fourthActionRow = new ActionRowBuilder().addComponents(ImageUrl);
+
+      modal2.addComponents(firstActionRow, secondActionRow, thirdActionRow, fourthActionRow);
+      interaction.showModal(modal2);
+
+      client.once('interactionCreate', async (interaction2) => {
+        if (!interaction2.isModalSubmit()) return;
+
+        if (interaction2.customId === 'modal2') {
+          try {
+            const Title = interaction2.fields.getTextInputValue('Title');
+            const Description = interaction2.fields.getTextInputValue('Description');
+            const Payment = interaction2.fields.getTextInputValue('Payment');
+            const ImageUrl = interaction2.fields.getTextInputValue('ImageUrl');
+
+            const memberInteractor = interaction2.user.id;
+
+            const channel = await client.channels.fetch(channelId);
+            const approvingChannel = await client.channels.fetch(approveChannelId);
+
+            const user = await interaction2.guild.members.fetch(memberInteractor);
+
+            const embed = new EmbedBuilder()
+              .setAuthor({
+                name: `${interaction2.user.username}`,
+                iconURL: interaction2.user.displayAvatarURL({ dynamic: true }),
+              })
+              .setTitle(`**Title:** ${Title}`)
+              .setDescription(`\n**Description:** ${Description}\n\n**Payment:** ${Payment}\n**Payment Type:** ${paymentType}\n\n**Contact** <@${memberInteractor}>`)
+              .setColor('cc33ff')
+              .setTimestamp();
+
+            if (ImageUrl) {
+              embed.setImage(ImageUrl);
+            }
+
             await approvingChannel.send({
               content: `<@${memberInteractor}>'s post\n\nBefore approving this, make sure this post has the proper grammar, details, payment, and more... If the post passes all of these then you may approve it`,
               embeds: [embed],
               components: [{ type: 1, components: [approveButton, declineButton] }]
             });
-  
-client.on('interactionCreate', async (interaction3) => {
+
+            client.on('interactionCreate', async (interaction3) => {
               if (!interaction3.isButton()) return;
 
               try {
@@ -88,75 +159,56 @@ client.on('interactionCreate', async (interaction3) => {
 
                   const expirationDate = new Date();
                   expirationDate.setDate(expirationDate.getDate() + 14); // Set the expiration date to 14 days from now
-client.on('interactionCreate', async (interaction3) => {
-  if (!interaction3.isButton()) return;
 
-  try {
-    if (interaction3.customId === 'approve_button') {
-      embed.setFooter({ text: `Approved by: ${interaction3.user.username}` });
+                  try {
+                    const expiredPost = new ExpiredPost({
+                      postId: postedMessage.id, // Use the posted message ID as the post ID or a unique identifier
+                      expirationDate,
+                    });
 
-      const channel = await client.channels.fetch(channelId);
-      const postedMessage = await channel.send({ content: `<@${memberInteractor}>`, embeds: [embed] });
+                    await expiredPost.save();
 
-      // Store the post information in a database or file for future reference
+                    interaction2.user.send({ content: `:tada: Your post was successfully approved, Congratulations! :tada:`, embeds: [embed] });
+                    interaction3.reply({ content: '<a:checkmark:1106657807740186684> Post was successfully approved' });
+                  } catch (error) {
+                    console.log(error);
+                    await interaction3.reply({ content: "<a:Xmark:1106658889656709202> An error occurred while processing your request. If this problem persists, please report it to us in <#1061732005533978684>", ephemeral: true });
+                  }
 
-      const expirationDate = new Date();
-      expirationDate.setDate(expirationDate.getDate() + 14); // Set the expiration date to 14 days from now
+                  // Schedule the deletion of the post after 14 days
+                  const expirationTimeout = setTimeout(async () => {
+                    // Retrieve the post information from the database using the postId
+                    const expiredPost = await ExpiredPost.findOne({ postId: postedMessage.id });
 
-      try {
-        const expiredPost = new ExpiredPost({
-          postId: postedMessage.id, // Use the posted message ID as the post ID or a unique identifier
-          expirationDate,
-        });
-        await expiredPost.save();
+                    if (expiredPost) {
+                      // Delete the post from the marketChannel
+                      await postedMessage.delete();
 
-        interaction2.user.send({ content: `:tada: Your post was successfully approved, Congratulations! :tada:`, embeds: [embed] });
-        interaction3.reply({ content: '<a:checkmark:1106657807740186684> Post was successfully approved' });
-      } catch (error) {
-        console.log(error);
-        await interaction3.reply({ content: "<a:Xmark:1106658889656709202> An error occurred while processing your request. If this problem persists, please report it to us in <#1061732005533978684>", ephemeral: true });
-      }
+                      // Remove the expired post document from the database
+                      await expiredPost.remove();
 
-      // Schedule the deletion of the post after 14 days
-      const expirationTimeout = setTimeout(async () => {
-        // Retrieve the post information from the database using the postId
-        const expiredPost = await ExpiredPost.findOne({ postId: postedMessage.id });
+                      await interaction2.user.send({
+                        content: `Your post with the id being: \`${expiredPost.postId}\` has been deleted from our market, as its been in there for 14 days (2 weeks).`,
+                        embeds: [embed],
+                      });
+                    }
+                  }, expirationDate - Date.now());
+                } else if (interaction3.customId === 'decline_button') {
+                  interaction3.reply(`Please enter the valid reason why you are declining this post`).then(() => {
+                    const filter = m => m.author.id === interaction3.user.id;
+                    const collector = interaction3.channel.createMessageCollector({ filter, max: 1 });
 
-        if (expiredPost) {
-          // Delete the post from the marketChannel
-          await postedMessage.delete();
-
-          // Remove the expired post document from the database
-          await expiredPost.remove();
-
-          await interaction2.user.send({
-            content: `Your post with the id being: \`${expiredPost.postId}\` has been deleted from our market, as its been in there for 14 days (2 weeks).`,
-            embeds: [embed],
-          });
-        }
-      }, expirationDate - Date.now());
-
-      // Store the expiration timeout in the database or file
-      const expirationTimeoutData = {
-        postId: postedMessage.id,
-        expirationTimeoutId: expirationTimeout,
-      };
-      await ExpiredPost.findOneAndUpdate({ postId: postedMessage.id }, { expirationTimeoutData }, { upsert: true });
-    } else if (interaction3.customId === 'decline_button') {
-      interaction3.reply(`Please enter the valid reason why you are declining this post`).then(() => {
-        const filter = m => m.author.id === interaction3.user.id;
-        const collector = interaction3.channel.createMessageCollector({ filter, max: 1 });
-
-        collector.on('collect', collected => {
-          const reason = collected.content;
-          interaction2.user.send({ content: `<a:Xmark:1106658889656709202> <@${memberInteractor}> Unfortunately your post in Bloxian Devs has been declined for the following reason: \`${reason}\``, embeds: [embed] });
-        });
-      });
-    }
-  } catch (error) {
-    console.log(error);
-    await interaction3.reply({ content: "<a:Xmark:1106658889656709202> An error occurred while processing your request. If this problem persists, please report it to us in <#1061732005533978684>", ephemeral: 
-   }
+                    collector.on('collect', collected => {
+                      const reason = collected.content;
+                      interaction2.user.send({ content: `<a:Xmark:1106658889656709202> <@${memberInteractor}> Unfortunately your post in Bloxian Devs has been declined for the following reason: \`${reason}\``, embeds: [embed] });
+                    });
+                  });
+                }
+              } catch (error) {
+                console.log(error);
+                await interaction3.reply({ content: "<a:Xmark:1106658889656709202> An error occurred while processing your request. If this problem persists, please report it to us in <#1061732005533978684>", ephemeral: true });
+              }
+            });
 
             cooldowns.set(interaction2.user.id, Date.now() + 12 * 60 * 60 * 1000); // 12 hours cooldown
             await interaction2.reply({ content: "<a:checkmark:1106657807740186684> Post was successfully submitted. Please wait patiently while our staff approves it.", ephemeral: false });
