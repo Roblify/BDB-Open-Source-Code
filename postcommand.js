141b98436fe67ccd79811357ade40f9857725b6a
Roblify <127715757+Roblify@users.noreply.github.com>
2023-06-12 23:51:18 -0700

    Update postcommand.js

--- postcommand.js
+++ postcommand.js
@@ -29,7 +29,7 @@ const commands = [
     .addStringOption(option => option.setName('job-type').setDescription('Select job type').addChoices({ name: 'For-Hire', value: 'for-hire' }, { name: 'Hiring', value: 'hiring' }, { name: 'Selling', value: 'selling' }).setRequired(true)),
 ];
 
-client.on('interactionCreate', async interaction => {
+  client.on('interactionCreate', async interaction => {
     if (!interaction.isCommand()) return;
 
     if (interaction.commandName === 'post') {
@@ -130,8 +130,8 @@ client.on('interactionCreate', async interaction => {
                 name: `${interaction2.user.username}`,
                 iconURL: interaction2.user.displayAvatarURL({ dynamic: true }),
               })
-              .setTitle(`**Title:** ${Title}`)
-              .setDescription(`\n**Description:** ${Description}\n\n**Payment:** ${Payment}\n**Payment Type:** ${paymentType}\n\n**Contact** <@${memberInteractor}>`)
+              .setTitle(`${Title}`)
+              .setDescription(`\n${Description}\n\n**Payment:** ${Payment}\n**Payment Type:** ${paymentType}\n\n**Contact** <@${memberInteractor}>`)
               .setColor('cc33ff')
               .setTimestamp();
 
@@ -145,70 +145,51 @@ client.on('interactionCreate', async interaction => {
               components: [{ type: 1, components: [approveButton, declineButton] }]
             });
 
-            client.on('interactionCreate', async (interaction3) => {
-              if (!interaction3.isButton()) return;
-
+        client.on('interactionCreate', async (interaction3) => {
+          if (!interaction3.isButton()) return;
+  
+          try {
+            if (interaction3.customId === 'approve_button') {
+              embed.setFooter({ text: `Approved by: ${interaction3.user.username}` });
+  
+              const channel = await client.channels.fetch(channelId);
+              postedMessage = await channel.send({ content: `<@${memberInteractor}>`, embeds: [embed] });
+  
+              // Store the post information in a database or file for future reference
+  
+              // const expirationDate = new Date();
+              // expirationDate.setDate(expirationDate.getDate() + 14); // Set the expiration date to 14 days from now
+  
               try {
-                if (interaction3.customId === 'approve_button') {
-                  embed.setFooter({ text: `Approved by: ${interaction3.user.username}` });
-
-                  const channel = await client.channels.fetch(channelId);
-                  const postedMessage = await channel.send({ content: `<@${memberInteractor}>`, embeds: [embed] });
-
-                  // Store the post information in a database or file for future reference
-
-                  const expirationDate = new Date();
-                  expirationDate.setDate(expirationDate.getDate() + 14); // Set the expiration date to 14 days from now
-
-                  try {
-                    const expiredPost = new ExpiredPost({
-                      postId: postedMessage.id, // Use the posted message ID as the post ID or a unique identifier
-                      expirationDate,
-                    });
-
-                    await expiredPost.save();
-
-                    interaction2.user.send({ content: `:tada: Your post was successfully approved, Congratulations! :tada:`, embeds: [embed] });
-                    interaction3.reply({ content: '<a:checkmark:1106657807740186684> Post was successfully approved' });
-                  } catch (error) {
-                    console.log(error);
-                    await interaction3.reply({ content: "<a:Xmark:1106658889656709202> An error occurred while processing your request. If this problem persists, please report it to us in <#1061732005533978684>", ephemeral: true });
-                  }
-
-                  // Schedule the deletion of the post after 14 days
-                  const expirationTimeout = setTimeout(async () => {
-                    // Retrieve the post information from the database using the postId
-                    const expiredPost = await ExpiredPost.findOne({ postId: postedMessage.id });
-
-                    if (expiredPost) {
-                      // Delete the post from the marketChannel
-                      await postedMessage.delete();
-
-                      // Remove the expired post document from the database
-                      await expiredPost.remove();
-
-                      await interaction2.user.send({
-                        content: `Your post with the id being: \`${expiredPost.postId}\` has been deleted from our market, as its been in there for 14 days (2 weeks).`,
-                        embeds: [embed],
-                      });
-                    }
-                  }, expirationDate - Date.now());
-                } else if (interaction3.customId === 'decline_button') {
-                  interaction3.reply(`Please enter the valid reason why you are declining this post`).then(() => {
-                    const filter = m => m.author.id === interaction3.user.id;
-                    const collector = interaction3.channel.createMessageCollector({ filter, max: 1 });
-
-                    collector.on('collect', collected => {
-                      const reason = collected.content;
-                      interaction2.user.send({ content: `<a:Xmark:1106658889656709202> <@${memberInteractor}> Unfortunately your post in Bloxian Devs has been declined for the following reason: \`${reason}\``, embeds: [embed] });
-                    });
-                  });
-                }
+                // const expiredPost = new ExpiredPost({
+                //postId: postedMessage.id, // Use the posted message ID as the post ID or a unique identifier
+                // expirationDate,
+                //});
+  
+                // await expiredPost.save();
+  
+                interaction.user.send({ content: `:tada: Your post was successfully approved, Congratulations! :tada:`, embeds: [embed] });
+                interaction3.reply({ content: '<a:checkmark:1106657807740186684> Post was successfully approved' });
               } catch (error) {
                 console.log(error);
                 await interaction3.reply({ content: "<a:Xmark:1106658889656709202> An error occurred while processing your request. If this problem persists, please report it to us in <#1061732005533978684>", ephemeral: true });
               }
-            });
+            } else if (interaction3.customId === 'decline_button') {
+              interaction3.reply(`Please enter the valid reason why you are declining this post`).then(() => {
+                const filter = m => m.author.id === interaction3.user.id;
+                const collector = interaction3.channel.createMessageCollector({ filter, max: 1 });
+  
+                collector.on('collect', collected => {
+                  const reason = collected.content;
+                  interaction.user.send({ content: `<a:Xmark:1106658889656709202> <@${memberInteractor}> Unfortunately your post in Bloxian Devs has been declined for the following reason: \`${reason}\``, embeds: [embed] });
+                });
+              });
+            }
+          } catch (error) {
+            console.log(error);
+            await interaction3.reply({ content: "<a:Xmark:1106658889656709202> An error occurred while processing your request. If this problem persists, please report it to us in <#1061732005533978684>", ephemeral: true });
+          }
+        });
 
             cooldowns.set(interaction2.user.id, Date.now() + 12 * 60 * 60 * 1000); // 12 hours cooldown
             await interaction2.reply({ content: "<a:checkmark:1106657807740186684> Post was successfully submitted. Please wait patiently while our staff approves it.", ephemeral: false });
@@ -219,6 +200,6 @@ client.on('interactionCreate', async interaction => {
         }
       });
     }
-});
+  });
 
 // All rights reserved. Do not steal the code from the open source repository.
